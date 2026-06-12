2fd05bea1e6ea0e57b54344faecb448af0fd510d
Ammar Hakimi <ammarsprogramming@gmail.com>
2026-06-12 08:11:08 +0000

    Add Terraform IaC for AWS infrastructure

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,12 @@
+aws_region   = "us-east-1"
+project_name = "sentinel-portal"
+
+db_name     = "sentineldb"
+db_username = "sentinel_admin"
+db_password = "SentinelPass2024!"
+
+encryption_key = "dR2beRg1azhSVMaUzUpr8U-Z8-7VZmJDIrtMkX7P_XU="
+
+enable_multi_az = false
+enable_https    = false
+acm_certificate_arn = ""
---
+++ terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  default = "sentinel-portal"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "db_name" {
+  default = "sentineldb"
+}
+
+variable "db_username" {
+  default = "sentinel_admin"
+}
+
+variable "db_password" {
+  sensitive = true
+  default   = "SentinelPass2024!"
+}
+
+variable "acm_certificate_arn" {
+  description = "ACM certificate ARN for HTTPS listener"
+  type        = string
+  default     = ""
+}
+
+variable "enable_multi_az" {
+  description = "Set true to attempt Multi-AZ RDS (not supported in Sandbox - used for Part E exploration)"
+  type        = bool
+  default     = false
+}
+
+variable "enable_https" {
+  description = "Set true only if a valid ACM certificate ARN is provided"
+  type        = bool
+  default     = false
+}
+variable "encryption_key" {
+  description = "Fernet key for application-level AES encryption"
+  sensitive   = true
+  type        = string
+  default     = ""
+}
